583ae3f7da245db5f8a405168ec520e2074fab27
Jeong Chaehong <brcps12@gmail.com>
2019-09-23 19:07:29 +0900

    project1: add include

---
+++ project1/include/time_chk.h
@@ -0,0 +1,32 @@
+#ifndef __TIME_CHK_H
+#define __TIME_CHK_H
+
+#include <sys/time.h>
+
+typedef struct {
+    struct timeval begin, end;
+    double interval;
+} time_interval_t;
+
+void begin_time_track(time_interval_t *interval) {
+    gettimeofday(&interval->begin, NULL);
+}
+
+void stop_time_track(time_interval_t *interval) {
+    gettimeofday(&interval->end, NULL);
+}
+
+void print_interval(time_interval_t *interval, const char *prefix) {
+    printf("%s: %'.3fms\n", prefix, (double) (interval->end.tv_sec - interval->begin.tv_sec) * 1000 + (double) (interval->end.tv_usec - interval->begin.tv_usec) / 1000);
+}
+
+void print_interval(time_interval_t *interval) {
+    print_interval(interval, "");
+}
+
+void stop_and_print_interval(time_interval_t *interval, const char *prefix) {
+    stop_time_track(interval);
+    print_interval(interval, prefix);
+}
+
+#endif
