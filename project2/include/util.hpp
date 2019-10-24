624a559e660cb732e39225b6d7c4bf5280edd742
Jeong Chaehong <brcps12@gmail.com>
2019-10-24 17:26:48 +0900

    project2: change random function

--- project2/include/util.hpp
+++ project2/include/util.hpp
@@ -2,10 +2,16 @@
 #define _UTIL_HPP
 
 #include <stdlib.h>
+#include <random>
+#include <ctime>
+#include <functional>
 
 // pick an integer randomly between begin and end inclusive
 int randomNumber(int begin, int end) {
-    return (end - begin + 1) * ((double)rand() / RAND_MAX) + begin;
+    thread_local std::mt19937 engine(std::random_device{}());
+    std::uniform_int_distribution<int> dist(begin, end);
+
+    return dist(engine);
 }
 
 #endif
